a03da9ae4b2b65053b60aba02dd6423fb72c24d1
sne-h <snehasahasnehasaha80@gmail.com>
2024-05-24 00:37:06 +0530

    Initial commit

---
+++ 1.UPPER_TRIANGULAR.py
@@ -0,0 +1,5 @@
+def lower_triangular(n):
+    for i in range(1, n + 1):
+        print('*' * i)
+
+lower_triangular(5)
---
+++ 2.LOWER_TRIANGULAR.py
@@ -0,0 +1,5 @@
+def upper_triangular(n):
+    for i in range(n, 0, -1):
+        print('*' * i)
+
+upper_triangular(5)
